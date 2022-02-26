60a8ce567e25173db79081c35fec47f94a95aec2
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-02-26 22:30:41 +0530

    formatted code

--- databases.py
+++ databases.py
@@ -35,7 +35,7 @@ class Database:
     def updateName(self, email, name):
         self.users.update_one({'email': email}, {'$set': {'username': name}})
         return True
-    
+
     def generateID(self):
         return "".join(random.choice("0123456789ABCDEF") for i in range(10))
 
@@ -59,7 +59,7 @@ class Database:
             "snippets": {},
             "snippetOrder": []
         }
-        
+
         self.books.update_one(
             {'_id': bookID}, {'$set': {'chapters.' + chapterID: chapter}})
         self.books.update_one(
@@ -76,44 +76,51 @@ class Database:
             {'_id': bookID}, {'$set': {'chapters.' + chapterID + '.snippets.' + snippetID: snippet}})
         self.books.update_one(
             {'_id': bookID}, {'$push': {'chapters.' + chapterID + '.snippetOrder': snippetID}})
-        
+
     def getBook(self, bookID):
         return self.books.find_one({'_id': bookID})
-    
+
     def getSnippet(self, bookID, chapterID, snippetID):
         book = self.books.find_one({'_id': bookID})
         return book['chapters'][chapterID]['snippets'][snippetID]
-    
+
     def changeChapterOrder(self, bookID, chapterOrder):
         self.books.update_one(
             {'_id': bookID}, {'$set': {'chapterOrder': chapterOrder}})
-    
+
     def changeSnippetOrder(self, bookID, chapterID, snippetOrder):
-        self.books.update_one({'_id': bookID}, {'$set': {'chapters.' + chapterID + '.snippetOrder': snippetOrder}})
-        
+        self.books.update_one({'_id': bookID}, {
+                              '$set': {'chapters.' + chapterID + '.snippetOrder': snippetOrder}})
+
     def deleteBook(self, bookID):
         book = self.books.find_one({'_id': bookID})
         self.books.delete_one({'_id': bookID})
-        self.users.update_one({'_id': book['by']}, {'$pull': {'books': bookID}})
-    
+        self.users.update_one({'_id': book['by']}, {
+                              '$pull': {'books': bookID}})
+
     def deleteChapter(self, bookID, chapterID):
-        self.books.update_one({'_id': bookID}, {'$unset': {'chapters.' + chapterID: 1}})
-        self.books.update_one({'_id': bookID}, {'$pull': {'chapterOrder': chapterID}})
-    
+        self.books.update_one(
+            {'_id': bookID}, {'$unset': {'chapters.' + chapterID: 1}})
+        self.books.update_one(
+            {'_id': bookID}, {'$pull': {'chapterOrder': chapterID}})
+
     def deleteSnippet(self, bookID, chapterID, snippetID):
-        self.books.update_one({'_id': bookID}, {'$unset': {'chapters.' + chapterID + '.snippets.' + snippetID: 1}})
-        self.books.update_one({'_id': bookID}, {'$pull': {'chapters.' + chapterID + '.snippetOrder': snippetID}})
-    
+        self.books.update_one({'_id': bookID}, {'$unset': {
+                              'chapters.' + chapterID + '.snippets.' + snippetID: 1}})
+        self.books.update_one({'_id': bookID}, {
+                              '$pull': {'chapters.' + chapterID + '.snippetOrder': snippetID}})
+
     def updateSnippetContent(self, bookID, chapterID, snippetID, content):
-        self.books.update_one({'_id': bookID}, {'$set': {'chapters.' + chapterID + '.snippets.' + snippetID + '.content': content}})
-        
+        self.books.update_one({'_id': bookID}, {'$set': {
+                              'chapters.' + chapterID + '.snippets.' + snippetID + '.content': content}})
+
     def updateSnippet(self, bookID, chapterID, snippetID, name):
-        self.books.update_one({'_id': bookID}, {'$set': {'chapters.' + chapterID + '.snippets.' + snippetID + '.name': name}})
-    
+        self.books.update_one({'_id': bookID}, {
+                              '$set': {'chapters.' + chapterID + '.snippets.' + snippetID + '.name': name}})
+
     def updateChapter(self, bookID, chapterID, name):
-        self.books.update_one({'_id': bookID}, {'$set': {'chapters.' + chapterID + '.name': name}})
-        
+        self.books.update_one(
+            {'_id': bookID}, {'$set': {'chapters.' + chapterID + '.name': name}})
+
     def updateBook(self, bookID, name):
         self.books.update_one({'_id': bookID}, {'$set': {'name': name}})
-    
-        
